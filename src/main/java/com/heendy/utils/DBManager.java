3275adc38083cb29d911adcdd80eefe2e4a00c1d
sisi397 <sieun0907@naver.com>
2022-03-05 18:31:41 +0900

    Feat: 환경셋팅

---
+++ src/main/java/com/heendy/utils/DBManager.java
@@ -0,0 +1,39 @@
+package com.heendy.utils;
+import java.sql.*;
+import javax.naming.*;
+import javax.sql.*;
+
+public class DBManager {
+	public static Connection getConnection() {
+		
+		Connection conn = null; //dbcp 
+		try {
+			Context initContext = new InitialContext();
+			Context envContext = (Context) initContext.lookup("java:/comp/env");
+			DataSource ds = (DataSource) envContext.lookup("jdbc/myoracle");
+			conn = ds.getConnection();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+
+	public static void close(Connection conn, Statement stmt, ResultSet rs) {
+		try {
+			rs.close();
+			stmt.close();
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void close(Connection conn, Statement stmt) {
+		try {
+			stmt.close();
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+} //end class
