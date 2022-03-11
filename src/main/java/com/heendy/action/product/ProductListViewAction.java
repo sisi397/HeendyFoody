064854b385a85e887e6091d95085eaeb61e1857c
sisi397 <sieun0907@naver.com>
2022-03-11 14:02:58 +0900

    Feat: 수정

--- src/main/java/com/heendy/action/product/ProductListViewAction.java
+++ src/main/java/com/heendy/action/product/ProductListViewAction.java
@@ -9,13 +9,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.heendy.action.Action;
 
-/** 
- * 
- * @author 김시은
- * 
- * 상품 목록 페이지로 이동
- *
- */
 public class ProductListViewAction implements Action {
 
 	@Override
