4961704f67227a73b9b40b222693148e34dcb46c
tmdwns <tmdwns1101@naver.com>
2022-03-12 15:20:20 +0900

    Feat : 주문, 장바구니 로그인 연동
    1. user정보를 저장,불러오는 인터페이스 추가
    
    2. 주문, 장바구니 API 필터 추가

--- src/main/java/com/heendy/action/order/OrderProductAction.java
+++ src/main/java/com/heendy/action/order/OrderProductAction.java
@@ -6,59 +6,63 @@ import java.sql.SQLException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.google.gson.Gson;
 import com.heendy.action.Action;
 import com.heendy.common.ErrorCode;
 import com.heendy.common.ErrorResponse;
 import com.heendy.common.SQLErrorCode;
+import com.heendy.common.exception.MemberNotExistSession;
 import com.heendy.dao.OrderDAO;
+import com.heendy.dto.MemberDTO;
 import com.heendy.dto.order.CreateOrderDTO;
+import com.heendy.utils.SessionUserService;
+import com.heendy.utils.UserService;
 
 public class OrderProductAction implements Action {
 
 	private final OrderDAO orderDAO = OrderDAO.getInstance();
-	
+
+	private UserService<MemberDTO, HttpSession> userService = SessionUserService.getInstance();
+
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
 		try {
 			response.setContentType("application/json");
 			response.setCharacterEncoding("utf-8");
 
-			/* 테스트용 멤버 id */
-			int memberId = 6;
-			
+			MemberDTO member = this.userService.loadUser(request.getSession()).orElseThrow(MemberNotExistSession::new);
+
 			int proudctId = Integer.parseInt(request.getParameter("product_id"));
 			int companyId = Integer.parseInt(request.getParameter("company_id"));
 			int count = Integer.parseInt(request.getParameter("product_count"));
-			
-			CreateOrderDTO createOrderDTO = new CreateOrderDTO(proudctId, companyId, memberId, count);
-			
+
+			CreateOrderDTO createOrderDTO = new CreateOrderDTO(proudctId, companyId, member.getMemberId(), count);
+
 			orderDAO.createOrder(createOrderDTO);
-			
+
 			response.setStatus(201);
 			response.getWriter().write("{\"created\" : true, \"result\" : \"주문이 완료 되었습니다.\"}");
-			
-		}catch(SQLException e) {
+
+		} catch (SQLException e) {
 			int errorCode = e.getErrorCode();
 			ErrorResponse errorResponse;
-			if(errorCode == SQLErrorCode.NO_DATA_FOUND.getCode()) {
+			if (errorCode == SQLErrorCode.NO_DATA_FOUND.getCode()) {
 				errorResponse = ErrorResponse.of(ErrorCode.NO_DATA_FOUND);
-			} else if(errorCode == SQLErrorCode.LACK_OF_STOCK.getCode()) {
+			} else if (errorCode == SQLErrorCode.LACK_OF_STOCK.getCode()) {
 				errorResponse = ErrorResponse.of(ErrorCode.LACK_OF_STOCK);
 			} else {
 				System.out.println(e.getMessage());
 				errorResponse = ErrorResponse.of(ErrorCode.UNCAUGHT_SERVER_ERROR);
 			}
-			
+
 			String json = new Gson().toJson(errorResponse);
 			response.setStatus(errorResponse.getStatus());
 			response.getWriter().write(json);
 		}
-		
+
 	}
-	
-	
 
 }
